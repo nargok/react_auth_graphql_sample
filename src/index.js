bd68e83fa045e93a4413aaeda7a97dacff089a5c
nargok <nargok@gmail.com>
2019-04-14 14:12:52 +0900

    apollo client settings

--- src/index.js
+++ src/index.js
@@ -3,19 +3,38 @@ import ReactDOM from 'react-dom';
 
 import { BrowserRouter } from "react-router-dom";
 import { Route } from "react-router";
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { ApolloClient } from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
 
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import Login from "./components/Login";
 
+const APP_BASE_URL = "http://localhost:8080/graphql";
+
+const httpLink = new HttpLink({
+  uri: APP_BASE_URL,
+});
+
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache,
+});
+
 ReactDOM.render(
-  <BrowserRouter>
-    <React.Fragment>
-      <Route exact="/" component={App} />
-      <Route path="/login" component={Login} />
-    </React.Fragment>
-  </BrowserRouter>,
+  <ApolloProvider client={client}>
+    <BrowserRouter>
+      <React.Fragment>
+        <Route exact="/" component={App} />
+        <Route path="/login" component={Login} />
+      </React.Fragment>
+    </BrowserRouter>
+  </ApolloProvider>,
   document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
